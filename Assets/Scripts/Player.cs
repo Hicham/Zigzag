767c02e8858c61d9ccc8a5eca47dcd09e54a122d
Hicham <hichamelans555@gmail.com>
2018-12-03 14:09:04 +0100

    V1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Vector2 targetPos;
+    private Vector2 targetPosUpwards;
+
+    private bool movementLR = false;
+
+    public float speed;
+    private float speedUpwards;
+
+    private float distance;
+
+    public float distanceLeft;
+    public float distanceRight;
+
+    private int left;
+    private int right;
+
+    private bool check_direction = true;
+
+
+    public float startSpeed;
+    public float MaxSpeedUpwards;
+
+    
+    public float speedMultiplier;
+
+    private bool check_firstAdd = true;
+    private float firstAdd;
+
+    void Start ()
+    {
+        
+    }
+	
+	
+	void Update ()
+    {
+        Gametime();
+        Movement();
+        
+    }
+
+    void Gametime()
+    {
+
+        if (speedUpwards < MaxSpeedUpwards)
+        {
+            speedUpwards = Time.time * speedMultiplier + firstAdd;
+           
+        }
+        else
+        {
+            speedUpwards = MaxSpeedUpwards;
+        }
+        Debug.Log(speedUpwards);
+
+
+        if (check_firstAdd == true)
+        {
+            firstAdd = startSpeed;
+        }
+        else if (check_firstAdd == false)
+        {
+            firstAdd = 0;
+            check_firstAdd = true;
+        }
+    }
+
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (check_direction == true)
+            {
+                left = -30;
+                right = 30;
+                check_direction = false;
+            }
+            else if (check_direction == false)
+            {
+                left = -60;
+                right = 60;
+            }
+
+            if (movementLR == false)
+            {
+                movementLR = true;
+                distance = distanceLeft;
+                Rotate(left);
+            }
+            else if (movementLR == true)
+            {
+                movementLR = false;
+                distance = distanceRight;
+                Rotate(right);
+            }
+            
+        }
+
+        targetPos = new Vector2(transform.position.x + distance, transform.position.y);
+        targetPosUpwards = new Vector2(transform.position.x, transform.position.y + 100);
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, targetPosUpwards, speedUpwards * Time.deltaTime);
+    }
+
+    void Rotate (float rotation)
+    {
+        transform.Rotate(Vector3.forward * rotation);
+    }
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+
+
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        
+        transform.position = new Vector3 (offset.x,  player.position.y + offset.y, offset.z);
+    }
+}
